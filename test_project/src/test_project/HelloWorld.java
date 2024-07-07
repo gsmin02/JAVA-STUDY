fe2de78eff27291bbdb97c58e1e17905c5853cc2
gsmin02 <gsmin5202@naver.com>
2024-07-07 22:54:21 +0900

    update

--- test_project/src/test_project/HelloWorld.java
+++ test_project/src/test_project/HelloWorld.java
@@ -3,8 +3,12 @@ package test_project;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+	
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println(a + b);
 	}
 
 }
