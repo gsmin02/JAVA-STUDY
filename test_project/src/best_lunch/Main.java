7d687bff7e9eaeda62ae145c53468f1e9ac1e51d
gsmin02 <gsmin5202@naver.com>
2024-07-09 16:42:18 +0900

    update

--- test_project/src/best_lunch/Main.java
+++ test_project/src/best_lunch/Main.java
@@ -1,10 +1,34 @@
 package best_lunch;
 
-public class Main {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello best_lunch!");
+		Scanner sc = new Scanner(System.in);
+		// 메뉴판 불러오기
+		Menu menu = new Menu();
+		int menu_len = menu.getMenu();
+		menu.printMenu();
+		
+		try {
+			// 입력 받기
+			int input = sc.nextInt();
+			// 입력받은 값 넘겨주기
+			new Process(input);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("0~" + (menu_len - 1) + "번까지 입력해주세요.");
+		}
+		catch (InputMismatchException e) {
+			System.out.println("숫자로 입력해주세요.");
+		}
+		catch (Exception e) {
+			System.out.println("알 수 없는 예외가 발생했습니다.");
+			System.out.println("원인 : " + e.getMessage());
+		}
+		
+		sc.close();
 	}
 
 }
